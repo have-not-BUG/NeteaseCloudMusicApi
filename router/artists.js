6a92b35b34c838b47fe18770ea363e6d93dedaae
have-not-BUG <lxyxdylglc@qq.com>
2017-11-13 11:07:06 +0800

    createWebAPIRequest

--- router/artists.js
+++ router/artists.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express()
-const { createWebAPIRequest } = require('../util/util')
+const  createWebAPIRequest  = require('../util/util')
 
 router.get('/', (req, res) => {
   const cookie = req.get('Cookie') ? req.get('Cookie') : ''
